e970ff64ccd9bb4fccee9c3f40eb4645a7e98b28
yahooserg <seltexinc@gmail.com>
2016-03-10 16:38:33 +0300

    Adding log via rest

--- 1.js
+++ 1.js
@@ -14,8 +14,8 @@
     });
 
     app.get('/test', function (req, res) {
-        var user = {id: 1},
-            connection = mysql.createConnection(mysqlConnection);
+
+        var connection = mysql.createConnection(mysqlConnection);
 
         connection.connect();
 
@@ -33,10 +33,13 @@
     });
 
     app.get('/api/storedata/store/:id/date/:date', function (req, res) {
-        var connection = new sql.Connection(mssqlConnection, function (err){
+
+        var connection = new sql.Connection(mssqlConnection, function (err) {
+
             var request = new sql.Request(connection),
-                 query = "select sum(c.Summa) as total, count(c.Summa) as checks from ChequeHead as c where convert(date,c.DateOperation) = '" + req.params.date + "' and c.Cash_Code = " + req.params.id;
-            request.query(query, function(err, recordset) {
+                query = "select sum(c.Summa) as total, count(c.Summa) as checks from ChequeHead as c where convert(date,c.DateOperation) = '" + req.params.date + "' and c.Cash_Code = " + req.params.id;
+
+            request.query(query, function (err, recordset) {
                 // console.info(recordset);
                 res.header("Content-Type", "application/json");
                 res.send(JSON.stringify(recordset[0]));
@@ -44,8 +47,23 @@
         });
     });
 
+    app.post('/api/log/user/:id/action/:action', function (req, res) {
+
+        var query = "call addLog(" + req.params.id + ", '" + req.params.action + "')",
+            connection = mysql.createConnection(mysqlConnection);
+            console.log(query);
+
+        connection.connect();
+
+        connection.query(query, function (err, rows, fields) {
+            console.info(rows);
+        });
+
+        connection.end();
+    });
+
     app.listen(5555, function () {
         var date = new Date();
-        console.log('fv server runs at 5555 ' + __dirname + " " + date);
+        console.log('fv server runs at 5555 ' + __dirname + ' ' + date);
     });
 }());

ba989894500852b6a368f7b0a1fd649f349acce8
yahooserg <seltexinc@gmail.com>
2019-12-08 18:12:46 +0300

    test

--- services/createrevenuedata.js
+++ services/createrevenuedata.js
@@ -41,7 +41,6 @@
     var msSQLJob = function (bakery, callback) {
       // console.log("in JOB: ", bakery);
       // Query
-      return 0;
       var query = "select sum(t1.nationalsum)as cash, count(t1.nationalsum) as checks, t1.IPRINTSTATION as cassa, day(t1.CLOSEDATETIME) as day, month(t1.CLOSEDATETIME) as month, DATEPART(dw,t1.CLOSEDATETIME) as dw from  [RK7].[dbo].[PRINTCHECKS] as t1 where year(t1.CLOSEDATETIME) = year(getdate()) and month(t1.CLOSEDATETIME) >= month(getdate())-2 and t1.IPRINTSTATION = " + bakery + " group by t1.IPRINTSTATION, day(t1.CLOSEDATETIME), month(t1.CLOSEDATETIME), DATEPART(dw,t1.CLOSEDATETIME) order by cassa, month desc, day desc;";
       const request = new sql.Request();
       request.query(query, (err, result) => {

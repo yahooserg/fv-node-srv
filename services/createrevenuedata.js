b31857b564c5a7483a5c5a06821f5584e7135dcb
yahooserg <seltexinc@gmail.com>
2019-12-10 16:51:10 +0300

    test

--- services/createrevenuedata.js
+++ services/createrevenuedata.js
@@ -14,7 +14,7 @@
     module.exports = {
         getDataFromDB: function (callback) {
 
-          var query = "select id, bakery, name from stores where bakery < 1000 order by bakery limit 10;",
+          var query = "select id, bakery, name from stores where bakery < 1000 order by bakery;",
           connection = mysql.createConnection(mysqlConnection),
           data = [];
           connection.connect();

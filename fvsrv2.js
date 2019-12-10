e1585f384328f4506730c7106ad999920f8e7746
yahooserg <seltexinc@gmail.com>
2019-12-10 17:52:09 +0300

    test save with all bakeries

--- fvsrv2.js
+++ fvsrv2.js
@@ -1,35 +1,13 @@
 /*jslint nomen: true, node: true, unparam: true*/
 (function () {
     "use strict";
-    var fs = require('fs');
-
-    // var express = require('express'),
-    //     app = express(),
-    //     http,
-    //     httpServer;
-    //
-    // http = require('http');
-    // httpServer = http.createServer(app);
-    // app.use(function (req, res, next) {
-    //     res.header("Access-Control-Allow-Origin", "*");
-    //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    //     next();
-    // });
-    // app.get('/api/test', function (req, res) {
-    //     console.log('test');
-    //     res.header("Content-Type", "application/json");
-    //     res.send(JSON.stringify({forecast: 'testFromInSRV', date: 2}));
-    // });
-    // httpServer.listen(5556, function () {
-    //     console.log('start');
-    // });
     var createRevenueData = require('./services/createrevenuedata.js'),
+      fs = require('fs'),
       revenueStore = function () {
         createRevenueData.getDataFromDB(function (data) {
           fs.writeFile("./../bakerydata.json", JSON.stringify(data), function () {
-            console.log(JSON.stringify(data));
+            console.log("DONE");
           })
-          // console.log(data);
         });
       };
     revenueStore();

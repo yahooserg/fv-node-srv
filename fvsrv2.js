5cb360e213b95b1b04930e131e1124561ad68e27
yahooserg <seltexinc@gmail.com>
2019-12-11 16:11:48 +0300

    test with date

--- fvsrv2.js
+++ fvsrv2.js
@@ -8,10 +8,11 @@
         var date = new Date();
         var currentDate = myFunctions.getDateString(date) + " " + myFunctions.getTimeString(date);
         // console.log(currentDate);
+
         console.log("Start: ", date);
         createRevenueData.getDataFromDB(function (data) {
+          data[date.length] = currentDate;
           fs.writeFile("./../bakerydata.json", JSON.stringify(data), function () {
-            // console.log("DONE");
             date = new Date();
             currentDate = myFunctions.getDateString(date) + " " + myFunctions.getTimeString(date);
             console.log("Finish: ", date);

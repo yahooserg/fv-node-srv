3d4293c742d977a198dcfdd2eded29d92666ae4a
yahooserg <seltexinc@gmail.com>
2019-12-11 16:41:12 +0300

    test

--- fvsrv2.js
+++ fvsrv2.js
@@ -3,38 +3,23 @@
     "use strict";
     var createRevenueData = require('./services/createrevenuedata.js'),
       fs = require('fs'),
-      myFunctions = require('./services/myfunctions');
-      // revenueStore = function () {
-      //   var date = new Date();
-      //   var currentDate = myFunctions.getDateString(date) + " " + myFunctions.getTimeString(date);
-      //   // console.log(currentDate);
-      //
-      //   console.log("Start: ", date);
-      //   createRevenueData.getDataFromDB(function (data) {
-      //     data[data.length] = currentDate;
-      //     fs.writeFile("./../bakerydata.json", JSON.stringify(data), function () {
-      //       date = new Date();
-      //       currentDate = myFunctions.getDateString(date) + " " + myFunctions.getTimeString(date);
-      //       console.log("Finish: ", date);
-      //       // console.log(currentDate);
-      //     })
-      //   });
-      // };
-    // revenueStore();
-    setInterval(function () {
-      var date = new Date();
-      var currentDate = myFunctions.getDateString(date) + " " + myFunctions.getTimeString(date);
-      // console.log(currentDate);
+      myFunctions = require('./services/myfunctions'),
+      revenueStore = function () {
+        var date = new Date();
+        var currentDate = myFunctions.getDateString(date) + " " + myFunctions.getTimeString(date);
+        // console.log(currentDate);
 
-      console.log("Start: ", date);
-      createRevenueData.getDataFromDB(function (data) {
-        data[data.length] = currentDate;
-        fs.writeFile("./../bakerydata.json", JSON.stringify(data), function () {
-          date = new Date();
-          currentDate = myFunctions.getDateString(date) + " " + myFunctions.getTimeString(date);
-          console.log("Finish: ", date);
-          // console.log(currentDate);
-        })
-      });
-    }, 120000);
+        console.log("Start: ", date);
+        createRevenueData.getDataFromDB(function (data) {
+          data[data.length] = currentDate;
+          fs.writeFile("./../bakerydata.json", JSON.stringify(data), function () {
+            date = new Date();
+            currentDate = myFunctions.getDateString(date) + " " + myFunctions.getTimeString(date);
+            console.log("Finish: ", date);
+            // console.log(currentDate);
+          })
+        });
+      };
+    // revenueStore();
+    setInterval(revenueStore, 60000);
 }());

5a71652cc9edf9d00b02413d9faadd0d6c430943
yahooserg <seltexinc@gmail.com>
2019-12-25 20:03:39 +0300

    bif test with no limit of bakeries

--- services/createrevenuedata.js
+++ services/createrevenuedata.js
@@ -15,7 +15,7 @@
         getDataFromDB: function (callback) {
 
 
-          var query = "select id, bakery, name from stores where bakery < 1000 order by bakery limit 2;",
+          var query = "select id, bakery, name from stores where bakery < 1000 order by bakery;",
           sqlQueryStore = "",
           connection = mysql.createConnection(mysqlConnection),
           data = [];

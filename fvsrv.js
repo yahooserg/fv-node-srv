f0bbb42a93abd5beb78f84a6623bc671e03c1dee
yahooserg <seltexinc@gmail.com>
2020-05-06 22:35:32 +0700

    hide get id bakery

--- fvsrv.js
+++ fvsrv.js
@@ -633,22 +633,24 @@
     //
     // setInterval(revenueStore, 3000);
 
-    var getBakeriesIDsFromServer = function () {
-      var config = require(__dirname + '/../dbconnectmssqlnode.js');
-      var query = "SELECT SIFR, CODE, NAME, NETNAME FROM CASHES order by sifr desc;";
-      sql.connect(config, err => {
-        const request = new sql.Request();
-        request.query(query, (err, result) => {
-            // ... error checks
-            if(err) {
-              console.log("Error: ", err);
-            }
-            console.log(result.recordset);
-        })
-      })
-
-    };
-
-    getBakeriesIDsFromServer();
+    ////////////////////////////////////
+    // THIS IS TO GET BAKERIES
+    // var getBakeriesIDsFromServer = function () {
+    //   var config = require(__dirname + '/../dbconnectmssqlnode.js');
+    //   var query = "SELECT SIFR, CODE, NAME, NETNAME FROM CASHES order by sifr desc;";
+    //   sql.connect(config, err => {
+    //     const request = new sql.Request();
+    //     request.query(query, (err, result) => {
+    //         // ... error checks
+    //         if(err) {
+    //           console.log("Error: ", err);
+    //         }
+    //         console.log(result.recordset);
+    //     })
+    //   })
+    //
+    // };
+    // getBakeriesIDsFromServer();
+    //////////////////////////////
 
 }());

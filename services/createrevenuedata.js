6f4c6f972daf17e256d517334034ee7dafa8ba52
yahooserg <seltexinc@gmail.com>
2021-05-26 11:40:21 +0700

    test

--- services/createrevenuedata.js
+++ services/createrevenuedata.js
@@ -67,11 +67,14 @@
             sqlQueryStore += ") ";
             var query = "select sum(t1.nationalsum)as cash, count(t1.nationalsum) as checks, t1.IPRINTSTATION as cassa, day(t1.CLOSEDATETIME) as day, month(t1.CLOSEDATETIME) as month, DATEPART(dw,t1.CLOSEDATETIME) as dw from  [RK7].[dbo].[PRINTCHECKS] as t1 where year(t1.CLOSEDATETIME) = year(getdate()) and month(t1.CLOSEDATETIME) >= month(getdate())-2 and " + sqlQueryStore + " group by t1.IPRINTSTATION, day(t1.CLOSEDATETIME), month(t1.CLOSEDATETIME), DATEPART(dw,t1.CLOSEDATETIME) order by cassa, month desc, day desc;";
             sql.connect(config, err => {
+              if(err) {
+                console.log("Error in connect: ", err);
+              }
               const request = new sql.Request();
               request.query(query, (err, result) => {
                   // ... error checks
                   if(err) {
-                    console.log("Error: ", err);
+                    console.log("Error in Data: ", err);
                   }
                   console.log("Data: ", results);
 
